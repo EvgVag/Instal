90882bae05aa2a320e87c9166fa6faf8f77aef8d
evgeny <evgeniyvaganov@yandex.ru>
2022-04-21 21:33:09 +0500

    commit

--- src/Main.java
+++ src/Main.java
@@ -3,53 +3,48 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class Main {
-
-    public static void main(String[] args) throws IOException {
-        File dir1 = new File("C:\\Users\\evgen\\Desktop\\Games\\src");
-        File underDir1 = new File(dir1 + "\\main");
-        File underDir12 = new File(dir1 + "\\test");
-        File dir2 = new File("C:\\Users\\evgen\\Desktop\\Games\\res");
-        File underDir21 = new File(dir2 + "\\drawables");
-        File underDir22 = new File(dir2 + "\\vectors");
-        File underDir23 = new File(dir2 + "\\icons");
-        File dir3 = new File("C:\\Users\\evgen\\Desktop\\Games\\savegames");
-        File dir4 = new File("C:\\Users\\evgen\\Desktop\\Games\\temp");
-        if(dir1.mkdir()) {
-            System.out.println(dir1.getName() + " создан");
+    public static StringBuilder sb = new StringBuilder();
+    public static void createDir(String adress) {
+        File dir = new File(adress);
+        if (dir.mkdir()) {
+            sb.append("Директория " + dir + " успешно создана" + "\n");
+        } else {
+            sb.append("Директория " + dir + " не создана" + "\n");
         }
-        underDir1.mkdir();
-        underDir12.mkdir();
-        dir2.mkdir();
-        underDir21.mkdir();
-        underDir22.mkdir();
-        underDir23.mkdir();
-        dir3.mkdir();
-        dir4.mkdir();
-        File file1 = new File(underDir1 + "\\Main.java");
-        File file2 = new File(underDir1 + "\\Utils.java");
-        File file3 = new File(dir4 + "\\temp.txt");
+    }
+
+    public static void createFile(String adress) {
+        File file = new File(adress);
         try {
-            file1.createNewFile();
-            file2.createNewFile();
-            file3.createNewFile();
+            if(file.createNewFile()){
+                sb.append("Файл " + file + " успешно создан" + "\n");
+            } else {
+                sb.append("Файл " + file + " не создан" + "\n");
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
-        StringBuilder sb = new StringBuilder();
-        sb.append("создан каталог src \n");
-        sb.append("создан подкаталог main \n");
-        sb.append("создан файл Main.java \n");
-        sb.append("создан файл Utils.java \n");
-        sb.append("создан подкаталог test \n");
-        sb.append("создан каталог res \n");
-        sb.append("создан подкаталог drawables \n");
-        sb.append("создан подкаталог vectors \n");
-        sb.append("создан подкаталог icons \n");
-        sb.append("создан каталог savegames \n");
-        sb.append("создан каталог temp \n");
-        sb.append("создан файл temp.txt \n");
+    }
 
-        FileWriter writer = new FileWriter(file3);
+    public static void main(String[] args) throws IOException {
+        createDir("C:\\Users\\evgen\\Desktop\\Games\\src");
+        createDir("C:\\Users\\evgen\\Desktop\\Games\\src\\main");
+        createDir("C:\\Users\\evgen\\Desktop\\Games\\src\\test");
+        createDir("C:\\Users\\evgen\\Desktop\\Games\\res");
+        createDir("C:\\Users\\evgen\\Desktop\\Games\\res\\drawables");
+        createDir("C:\\Users\\evgen\\Desktop\\Games\\res\\vectors");
+        createDir("C:\\Users\\evgen\\Desktop\\Games\\res\\icons");
+        createDir("C:\\Users\\evgen\\Desktop\\Games\\savegames");
+        createDir("C:\\Users\\evgen\\Desktop\\Games\\temp");
+
+        createFile("C:\\Users\\evgen\\Desktop\\Games\\src\\main\\Main.java");
+        createFile("C:\\Users\\evgen\\Desktop\\Games\\src\\main\\Utils.java");
+        createFile("C:\\Users\\evgen\\Desktop\\Games\\temp\\temp.txt");
+
+
+
+
+        FileWriter writer = new FileWriter("C:\\Users\\evgen\\Desktop\\Games\\temp\\temp.txt");
         writer.write(String.valueOf(sb));
         writer.flush();
         writer.close();
